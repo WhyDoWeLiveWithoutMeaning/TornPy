b4a873f08d83b7f4e4ce01e8fa0fb022018c3f2b
Eric Beaulne <Eric.minecraft20@gmail.com>
2026-01-14 03:44:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
   name='TornPy',
-  version='0.0.1',
+  version='0.0.2',
   description='A Wrapper for the Torn API',
   author="Meaning",
   long_description=long_description,
   long_description_content_type="text/markdown",
   license="MIT",
-  packages=['torn'],
+  packages=find_packages(),
   install_requires=[
     "pydantic>=2.12.5",
     "httpx>=0.28.1",
